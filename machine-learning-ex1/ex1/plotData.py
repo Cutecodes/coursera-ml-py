7c3cb29035148a5d0ad5510705a2452dbe877d6d
Cutecodes <48645929+Cutecodes@users.noreply.github.com>
2021-02-10 19:53:30 +0800

    Add files via upload

--- machine-learning-ex1/ex1/plotData.py
+++ machine-learning-ex1/ex1/plotData.py
@@ -13,5 +13,9 @@ def plot_data(x, y):
 
 
     # ===========================================================
+    plt.scatter(x,y)
+    plt.xlabel('Population of City in 10,000s')
+    plt.ylabel('Profit in $10,000s')
 
     plt.show()
+
